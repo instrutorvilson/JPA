720abd54add30cbde8ce3736eb36c37b1d21b2b4
Vilson Moro <vilsonmoro@gmail.com>
2020-11-28 21:29:34 -0200

    Consulta com JPQL

--- src/DAO/Dao.java
+++ src/DAO/Dao.java
@@ -5,6 +5,9 @@
  */
 package DAO;
 
+import entidades.Carro;
+import entidades.Cliente;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,6 +17,26 @@ import javax.persistence.Persistence;
  * @author User
  */
 public class Dao {
+    
+    public static Carro consultaByPlaca(String pPlaca){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-BD-APPPU");
+        EntityManager em = emf.createEntityManager();
+       /* return em.createNamedQuery("Carro.findByPlaca", Carro.class)
+               .setParameter("placa", pPlaca)
+               .getSingleResult(); */
+        String jpql = "SELECT c FROM Carro c WHERE c.placa = :placa";
+        return em.createQuery(jpql, Carro.class)
+          .setParameter("placa", pPlaca)
+          .getSingleResult();
+    }
+    
+    public static List<Carro> consultaCarros(){
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-BD-APPPU");
+       EntityManager em = emf.createEntityManager();
+       return em.createNamedQuery("Carro.findAll", Carro.class)
+                .getResultList();
+    }
+           
 
     public static boolean persist(Object object) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-BD-APPPU");
@@ -31,4 +54,23 @@ public class Dao {
         return true;
     }
     
-}
+    public static Cliente selecionarByCPF(String cpf){
+      EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-BD-APPPU");
+      EntityManager em = emf.createEntityManager(); 
+     /* TypedQuery<Cliente> tq = em.createNamedQuery("Cliente.findByCpf", Cliente.class);
+      tq.setParameter("cpf", cpf);
+      return tq.getSingleResult();*/
+     return em.createNamedQuery("Cliente.findByCpf", Cliente.class)
+              .setParameter("cpf", cpf)
+              .getSingleResult();
+    }
+
+    public static List<Cliente> selecionarTodos(){
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-BD-APPPU");
+       EntityManager em = emf.createEntityManager(); 
+       return em.createNamedQuery("Cliente.findAll", Cliente.class)
+                .getResultList();
+    }
+
+}    
+
